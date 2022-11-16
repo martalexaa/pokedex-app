0161fcec10eeb732559ac66ae85d57e23b254248
martalexaa <63308375+martalexaa@users.noreply.github.com>
2022-11-16 11:40:55 +0100

    pokemonList added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pokedex-App
+
+Project description
+How to get the project running
+Project dependencies (e.g., JavaScript version, ESLint rules)
+Which API the project uses
+
+Project Brief: https://images.careerfoundry.com/public/courses/fullstack-immersion/Full-Stack%20Immersion%20A1%20Project%20Brief.pdf
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-var favoriteFood = 'pizza'
-document.write(favoriteFood)
+//List of all Pokemons in this app
+let pokemonList = [
+  { name: 'Pikachu', types: 'electric', height: 0.4, weight: 6 },
+  { name: 'Raichu', types: 'electric', height: 0.8, weight: 30 },
+  { name: 'Squirtle', types: 'water', height: 0.5, weight: 9 },
+  { name: 'Nidoran', types: 'poison', height: 0.4, weight: 7 },
+];
