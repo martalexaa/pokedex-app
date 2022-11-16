dc9b7a4e1017d5587692615fee84684da4954875
martalexaa <63308375+martalexaa@users.noreply.github.com>
2022-11-16 12:18:19 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //List of all Pokemons in this app
 let pokemonList = [
-  { name: 'Pikachu', types: 'electric', height: 0.4, weight: 6 },
-  { name: 'Raichu', types: 'electric', height: 0.8, weight: 30 },
-  { name: 'Squirtle', types: 'water', height: 0.5, weight: 9 },
-  { name: 'Nidoran', types: 'poison', height: 0.4, weight: 7 },
+  { name: 'Pikachu', height: 0.4, weight: 6, types: ['electric'] },
+  { name: 'Raichu', height: 0.8, weight: 30, types: ['electric'] },
+  { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] },
+  { name: 'Nidoran', height: 0.4, weight: 7, types: ['poison'] },
 ];
