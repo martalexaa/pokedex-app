e44cd5527522016ca0c26198b29327321dba2ddc
martalexaa <63308375+martalexaa@users.noreply.github.com>
2022-11-17 12:29:19 +0100

    Pokemon names followed by heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 //List of all Pokemons in this app
 let pokemonList = [
-  { name: 'Pikachu', height: 0.4, weight: 6, types: ['electric'] },
-  { name: 'Raichu', height: 0.8, weight: 30, types: ['electric'] },
-  { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] },
-  { name: 'Nidoran', height: 0.4, weight: 7, types: ['poison'] },
+  { 
+    name: 'Pikachu', 
+    height: 0.4, 
+    weight: 6, 
+    types: ['electric'] 
+},
+  { 
+    name: 'Raichu', 
+    height: 0.8, 
+    weight: 30, 
+    types: ['electric'] 
+},
+  { name: 'Squirtle', 
+  height: 0.5, 
+  weight: 9, 
+  types: ['water'] 
+},
+  { name: 'Nidoran', 
+  height: 0.4, 
+  weight: 7, 
+  types: ['poison'] 
+},
 ];
+
+//Prints to the document each Pokemon's name followed by height
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' ' + '(height: '), 
+    document.write(pokemonList[i].height + ')<br>');
+    
+//Highlights the biggest Pokemon which size is above 0.7
+  if (pokemonList[i].height > 0.7) {document.write('- Wow, this one is the biggest!' + '<br>')};
+  }
+
+
