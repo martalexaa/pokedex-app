1245b079d13b5e0a79192b4d8a941c37f43f6052
martalexaa <63308375+martalexaa@users.noreply.github.com>
2022-11-22 13:47:20 +0100

    Update scripts.js with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -24,13 +24,26 @@ let pokemonList = [
 },
 ];
 
-//Prints to the document each Pokemon's name followed by height
+//Iterates over the each pokemon in pokemonList using forEach loop, highlights the biggest one
+
+function myLoopFunction(pokemon) {
+  document.write('<p>' + pokemon.name 
+  + ' (height: ' + pokemon.height + ' m),' 
+  + ' (weight: ' + pokemon.weight + ' kg),' 
+  + ' type: ' + pokemon.types);
+  if (pokemon.height > 0.7) {
+    document.write('<span>' + '  - Wow, this one is the biggest!' + '</span>')
+  };  
+}
+  pokemonList.forEach(myLoopFunction);
+
+/*Prints to the document each Pokemon's name followed by height
 for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name + ' ' + '(height: '), 
     document.write(pokemonList[i].height + ')<br>');
     
-//Highlights the biggest Pokemon which size is above 0.7
+Highlights the biggest Pokemon which size is above 0.7
   if (pokemonList[i].height > 0.7) {document.write('- Wow, this one is the biggest!' + '<br>')};
-  }
+  }*/
 
 
