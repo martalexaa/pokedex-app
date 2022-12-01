c9f2f86caaf14d3c45f734ebd8be0c50866e0361
martalexaa <63308375+martalexaa@users.noreply.github.com>
2022-12-01 13:39:41 +0100

    Modal window

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,64 @@
+//IIFE
 let pokemonRepository = (function () {
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=80';
+
+let modalContainer = document.querySelector('#modal-container');
+
+//a function to open the modal container when the user click on one of the pokemon buttons
+function showModal(title, text, img) {
+modalContainer.innerHTML = '';
+
+let modal = document.createElement('div');
+modal.classList.add('modal');
+
+// Add the new modal content
+let closeButtonElement = document.createElement('button');
+closeButtonElement.classList.add('modal-close');
+closeButtonElement.innerText = 'Close';
+closeButtonElement.addEventListener('click', hideModal);
+
+let titleElement = document.createElement('h1');
+titleElement.innerText = title;
+
+let contentElement = document.createElement('p');
+contentElement.innerText = text;
+
+let imageElement = document.createElement("img");
+imageElement.setAttribute("src", img);
+imageElement.setAttribute("width", "80%");
+imageElement.setAttribute("height", "80%");
+imageElement.setAttribute("alt", "Pokemon image");
+imageElement.classList.add("pokemon-image");
+//imageElement.src = item.imageUrl; //why does it not work?? if I uncomment this, the modal does not open
+
+modal.appendChild(closeButtonElement);
+modal.appendChild(titleElement);
+modal.appendChild(contentElement);
+modal.appendChild(imageElement);
+modalContainer.appendChild(modal);
+
+modalContainer.classList.add('is-visible');
+}
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+});
+
+modalContainer.addEventListener('click', (e) => {
+  // Since this is also triggered when clicking INSIDE the modal container,
+  // We only want to close if the user clicks directly on the overlay
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
 
 function add(pokemon) {
   if (
@@ -13,35 +71,36 @@ function add(pokemon) {
   }
 }
 
-  function getAll() {
-    return pokemonList;
-  }
+function getAll() {
+  return pokemonList;
+}
 
-  function forEach(pokemon) {
-    return pokemonList.forEach(pokemon);
-  }
+function forEach(pokemon) {
+  return pokemonList.forEach(pokemon);
+}
 
-  function addListItem(pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
+//creating a list of buttons with pokemon names
+function addListItem(pokemon) {  
+  let pokemonList = document.querySelector('.pokemon-list');
     //creates a list element
-    let listItem = document.createElement('li');
+  let listItem = document.createElement('li');
     //creates a button
-    let button = document.createElement('button');
+  let button = document.createElement('button');
     //adds text to the button
-    button.innerText = pokemon.name;
+  button.innerText = pokemon.name;
     //adds a class to the button, in order to be able to add to it style in css
-    button.classList.add('mybutton');
+  button.classList.add('mybutton');
     //adds the button as a child element to listItem parent element
-    listItem.appendChild(button);
+  listItem.appendChild(button);
     //adds the listItem as a child element to it's parent, the pokemonList
-    pokemonList.appendChild(listItem);
+  pokemonList.appendChild(listItem);
     //adds a button function
-    button.addEventListener('click', function () {
+  button.addEventListener('click', function () {
       showDetails(pokemon);
     });
   }
   
-  //fetching the data from URL
+  //fetching the pokemon data from URL
     function loadList() {
       return fetch(apiUrl).then(function (response) {  // <== this is a promise
         return response.json();  // <== the data is transferred from the server in JSON language
@@ -49,7 +108,7 @@ function add(pokemon) {
         json.results.forEach(function (item) {
           let pokemon = {
             name: item.name,
-            detailsUrl: item.url
+            detailsUrl: item.url,
           };
           add(pokemon);
         });
@@ -58,10 +117,10 @@ function add(pokemon) {
       })
     }
   
-  //function for loading details
-    function loadDetails(item) {
-      let url = item.detailsUrl;
-      return fetch(url).then(function (response) {
+//function for loading details of each Pokemon
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
         return response.json();
       }).then(function (details) {
         // adding the details to the item
@@ -74,11 +133,16 @@ function add(pokemon) {
     }
 
     function showDetails(item) {
+      showModal(item.name, item.name + ' is ' + item.height + ' high. ' + 'Type: ' + item.types, item.imageUrl); //why does the height and type not show???
+   };
+
+    /*function showDetails(item) {
       pokemonRepository.loadDetails(item).then(function () {
-        console.log(item);
+        console.log(pokemon.name, 'height: ' + pokemon.height);
       });
-    }
-  
+    }*/
+    
+    //functions returned by IIFE 
     return {
       add: add,
       getAll: getAll,
@@ -86,9 +150,13 @@ function add(pokemon) {
       forEach: forEach,
       addListItem: addListItem,
       loadDetails: loadDetails,
-      showDetails: showDetails
+      showDetails: showDetails,
+      showModal: showModal,
+      hideModal: hideModal
     };
 })();
+
+//FROM HERE WE ARE OUTSIDE OF IIFE
 
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
